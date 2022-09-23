fa65f7ad8a729d7abfa50f8e2e21a9d8cabbff13
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-09-23 11:57:48 +0800

    Add files via upload

--- test.java
+++ test.java
@@ -2,6 +2,9 @@ public class test{
 	public static void main(String[] args){
 		System.out.println(args[0]);
 		System.out.println(args[1]);
-
+		printStr("aaa");
 	}    
+	public static void printStr(String str){
+		System.out.println("My name is " + str);
+	}
 }
