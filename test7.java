45a8bbfce58187b9646aec5d3b59d189836359fd
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-10-07 11:43:30 +0800

    Add files via upload

--- test7.java
+++ test7.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
 
 public class test7 {
     public static void main(String[] args) {
         int grade = 9;
 
-        while (grade <= 0) {
+        while (grade >= 0) {
             // Thread.currentThread().sleep(1000);
 
-            System.out.printf("%d", grade);
+            System.out.printf("%d\n", grade);
             grade--;
         }
 
