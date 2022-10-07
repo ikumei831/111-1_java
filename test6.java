4fc071eb6a621db0b6dafe169951b1d6ca7f6106
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-10-07 09:44:59 +0800

    Add files via upload

--- test6.java
+++ test6.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
-public class test6{
-	public static void main(String[] args){
-		Scanner input= new Scanner(System.in);
+
+public class test6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("grade:");
-		int grade= input.nextInt();
-        
-        switch(grade/10*10){
-            case 90:
-                System.out.println("B");
-                break;
-            case 80:
-                System.out.println("B");
-                break;
-            case 70:
-                System.out.println("C");
-                break; 
-            case 60:
-                System.out.println("D");
-                break;
-            default:
-                System.out.println("E");
-                break;
-        }
-	}    
-	public static void printStr(int str){
-		System.out.println("ep " + sum);
-	}
+        int grade = input.nextInt();
+        if (grade <= 100) {
+            switch (grade / 10 * 10) {
+                case 100:
+                case 90:
+                    System.out.println("A");
+                    break;
+                case 80:
+                    System.out.println("B");
+                    break;
+                case 70:
+                    System.out.println("C");
+                    break;
+                case 60:
+                    System.out.println("D");
+                    break;
+                default:
+                    System.out.println("E");
+                    break;
+            }
+        } else
+            System.out.println("not in value");
+    }
+
 }
