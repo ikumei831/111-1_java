1758c170e57bc525c66aa0fc53815bc78a8bd0df
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-10-07 11:11:21 +0800

    Add files via upload

---
+++ test7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test7 {
+    public static void main(String[] args) {
+        int grade = 9;
+
+        while (grade <= 0) {
+            // Thread.currentThread().sleep(1000);
+
+            System.out.printf("%d", grade);
+            grade--;
+        }
+
+    }
+
+}
