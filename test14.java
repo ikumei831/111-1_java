20d8e5f685a320f80a6cc73e88f83b5b6a82c667
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-10-28 11:53:00 +0800

    Add files via upload

---
+++ test14.java
@@ -0,0 +1,16 @@
+public class test14 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 6; j >= 1; j--) {
+
+                if (i < j)
+                    System.out.print("*");
+                else
+
+                    System.out.print(i - j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
